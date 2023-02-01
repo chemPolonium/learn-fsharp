c425ef42ca932ae906f12580e0597a8752ae0638
chemPolonium <chempolonium@outlook.com>
2023-02-02 00:23:39 +0800

    add 32

---
+++ lt32/Program.fs
@@ -0,0 +1,16 @@
+﻿//object expression
+type IA =
+    abstract MyFunction: int -> int
+    abstract MyValue: string with get, set
+
+let myFunction i = i + 1
+//这个不能写在OE里面
+let mutable str = "Hello"
+
+let a =
+    { new IA with
+        member _.MyFunction i = myFunction i
+
+        member _.MyValue
+            with get () = str
+            and set (i) = str <- i }
