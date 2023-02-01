cd95d15bb55fa590e68d3981112ce90b8d7652eb
chemPolonium <chempolonium@outlook.com>
2023-02-01 20:53:50 +0800

    add 31

--- lt31/Program.fs
+++ lt31/Program.fs
@@ -1,5 +1,7 @@
 ﻿type IA =
     abstract member MyValue2: string with get, set
+//接口所有成员必须显式实现
+    abstract member MyValue: string with get, set
 
 type MyBase() =
     member val MyValue = "MyString" with get, set
@@ -11,8 +13,12 @@ type MyClass() =
         member _.MyValue2
             with get () = base.MyValue
             and set (i) = base.MyValue <- i
+        member _.MyValue
+            with get () = base.MyValue
+            and set (i) = base.MyValue <- i
 
 let mc = MyClass()
 
 let b = mc :> IA
+printfn "%s" b.MyValue
 printfn "%s" b.MyValue2
