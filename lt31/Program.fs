4bed2dee43112886850444e56b273975922f2e52
chemPolonium <chempolonium@outlook.com>
2023-02-01 20:33:42 +0800

    add 31

---
+++ lt31/Program.fs
@@ -0,0 +1,18 @@
+﻿type IA =
+    abstract member MyValue2: string with get, set
+
+type MyBase() =
+    member val MyValue = "MyString" with get, set
+
+type MyClass() =
+    inherit MyBase()
+
+    interface IA with
+        member _.MyValue2
+            with get () = base.MyValue
+            and set (i) = base.MyValue <- i
+
+let mc = MyClass()
+
+let b = mc :> IA
+printfn "%s" b.MyValue2
