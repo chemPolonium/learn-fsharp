494be32ae47ccfa99f1836ddb19ce055f2257e4e
chemPolonium <chempolonium@outlook.com>
2023-02-02 21:39:58 +0800

    add 37

---
+++ lt37/Program.fs
@@ -0,0 +1,16 @@
+﻿let rec qs list cont =
+    match list with
+    | [] -> cont ([])
+    | [ a ] -> cont ([ a ])
+    | head :: tail ->
+        let lessList = tail |> List.filter (fun i -> i <= head)
+        let moreList = tail |> List.filter (fun i -> i > head)
+
+        qs lessList (fun lessListPara ->
+            qs moreList (fun moreListPara -> cont (lessListPara @ [ head ] @ moreListPara)))
+
+let list = [ 0; 7; 2; 6; 8; 4; 1; 12 ]
+let result = qs list id
+
+for i in result do
+    printfn "%A" i
