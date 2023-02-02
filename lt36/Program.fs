2dd6277bfc9475219457dfd64ef3648b5676a3c0
chemPolonium <chempolonium@outlook.com>
2023-02-02 15:10:38 +0800

    add 36

---
+++ lt36/Program.fs
@@ -0,0 +1,14 @@
+﻿let l = [ 1L .. 1000000L ]
+
+//continuous passing style (CPS)
+//continuous programing style (CPS)
+let rec sum l cont =
+    match l with
+    | [] -> cont 0L
+    | h :: t -> sum t (fun x -> cont (h + x))
+
+let y = sum l id
+printfn "%A" y
+
+let z = sum l (fun x -> x + 1L)
+printfn "%A" z
