862102750ac7d0bed74f0bf9e4ddcd5d83758368
chemPolonium <chempolonium@outlook.com>
2023-02-02 21:50:21 +0800

    mod 37

--- lt37/Program.fs
+++ lt37/Program.fs
@@ -1,4 +1,4 @@
-﻿let rec qs list cont =
+﻿let rec qs1 list cont =
     match list with
     | [] -> cont ([])
     | [ a ] -> cont ([ a ])
@@ -6,11 +6,22 @@
         let lessList = tail |> List.filter (fun i -> i <= head)
         let moreList = tail |> List.filter (fun i -> i > head)
 
-        qs lessList (fun lessListPara ->
-            qs moreList (fun moreListPara -> cont (lessListPara @ [ head ] @ moreListPara)))
+        qs1 lessList (fun lessListPara ->
+            qs1 moreList (fun moreListPara -> cont (lessListPara @ [ head ] @ moreListPara)))
+
+//without CPS
+let rec qs2 list =
+    match list with
+    | [] -> []
+    | [ a ] -> [ a ]
+    | head :: tail ->
+        let lessList = tail |> List.filter (fun i -> i <= head)
+        let moreList = tail |> List.filter (fun i -> i > head)
+        qs2 lessList @ [ head ] @ qs2 moreList
 
 let list = [ 0; 7; 2; 6; 8; 4; 1; 12 ]
-let result = qs list id
+let result1 = qs1 list id
+let result2 = qs2 list
 
-for i in result do
-    printfn "%A" i
+printfn "%A" result1
+printfn "%A" result2
