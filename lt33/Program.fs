0781792cb683cd22b86cee9db809bb881acf6f44
chemPolonium <chempolonium@outlook.com>
2023-02-02 13:55:07 +0800

    add 33

--- lt33/Program.fs
+++ lt33/Program.fs
@@ -1,2 +1,6 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type Patterns =
+    | BigNumber of int
+    | HugeNumber
+
+printfn "%s" (nameof (BigNumber))
+printfn "%s" "BigNumber"
