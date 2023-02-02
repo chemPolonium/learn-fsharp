e93b7bc3e44cdf7c35ef8f49a7da16c5dd6fc360
chemPolonium <chempolonium@outlook.com>
2023-02-02 14:42:14 +0800

    add 34

---
+++ lt34/Program.fs
@@ -0,0 +1,14 @@
+﻿type Customer(firstName, middleInitial, lastName) =
+    member _.FirstName = firstName
+    member _.MiddleInitial = middleInitial
+    member _.LastName = lastName
+
+    member this.SayFullName() =
+        $"{this.FirstName} {this.MiddleInitial} {this.LastName}"
+
+let customer = Customer("Emillia", "C", "Miller")
+
+//这个会报错，属性不能修改
+//customer.FirstName <- "NewFirstName"
+
+printfn $"Hello, I'm {customer.SayFullName()}!"
