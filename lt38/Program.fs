81143456998510a6094d2453fdb9641dd335c14f
chemPolonium <chempolonium@outlook.com>
2023-02-03 13:37:19 +0800

    add 38

---
+++ lt38/Program.fs
@@ -0,0 +1,20 @@
+﻿type Variant =
+    | HugeNumber of int
+    | BigNumber
+    | SmallNumber
+
+module FunctionLibrary =
+    let print =
+        function
+        | HugeNumber n -> printfn "Num %d" n
+        | BigNumber -> printfn $"nameof{BigNumber}"
+        | SmallNumber -> printfn "Small number"
+
+type Variant with
+
+    member x.Print() = FunctionLibrary.print x
+
+let a = Variant.SmallNumber
+let b = Variant.HugeNumber 100
+a |> FunctionLibrary.print
+b.Print()
