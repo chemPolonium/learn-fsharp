8087a24d70793e0f5271d0d712919507dac9820b
chemPolonium <chempolonium@outlook.com>
2023-02-17 15:08:09 +0800

    add 05

---
+++ lt05/Program.fs
@@ -0,0 +1,10 @@
+﻿// 05 F# 的入口点
+
+// F# 没有入口点的时候是顺序执行的
+
+let ask student ``a question`` =
+    printf "me ask %s: %s" student ``a question``
+
+let askJohn = ask "John"
+
+askJohn "How old are you?"
