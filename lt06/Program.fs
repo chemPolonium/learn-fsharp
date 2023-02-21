3024080c9ba9b9d6c838d9f7b67edde6c2780fd1
chemPolonium <chempolonium@outlook.com>
2023-02-21 19:37:24 +0800

    add 06 07

---
+++ lt06/Program.fs
@@ -0,0 +1,18 @@
+﻿// 06 含有入口点的 F# 程序执行顺序
+
+let debug = printfn "reach here %d"
+
+debug 1
+
+let from whom = sprintf "from %s" whom
+
+debug 2
+
+[<EntryPoint>]
+let main argv =
+    printfn "entry main"
+    let message = from "F#"
+    printfn "Hello world %s" message
+    0
+
+// EntryPoint 后面不能写新东西了
---
+++ lt07/Program.fs
@@ -0,0 +1,11 @@
+﻿// 07 对于函数的操作
+
+let f x = 2
+
+let print x = printfn "check here %d" x
+
+let print2 _ = print 2
+
+print 4
+print2 4
+print2 ()
