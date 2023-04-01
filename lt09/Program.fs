76fdff1ee995daf6337773ff1b02dc433748dfef
chemPolonium <chempolonium@outlook.com>
2023-04-01 23:34:55 +0800

    09

---
+++ lt09/Program.fs
@@ -0,0 +1,14 @@
+﻿let ``inc 1`` = (+) 1
+let toKiloMeter = (*) 1.6
+let ``20 div`` = (/) 20
+
+let 开方 = sqrt
+let 乘十 = (*) 10.
+let 开方乘十 = 开方 >> 乘十
+
+let result = ``inc 1`` 50
+printfn $"result = {result}"
+
+// 在这里要写 36.0 而不是 36
+let result2 = 开方乘十 36.
+printfn $"result = {result2}"
