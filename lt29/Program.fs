7169b8a9003083939abe1705bfdc4b49f7d65b0f
chemPolonium <chempolonium@outlook.com>
2023-05-04 17:59:21 +0800

    29 comment

--- lt29/Program.fs
+++ lt29/Program.fs
@@ -1,6 +1,9 @@
-﻿type HumanName = HumanName of string
+﻿// 29 通过联合重新声明一个类型
+
+type HumanName = HumanName of string
 type DogName = DogName of string
 
+// 此时这个函数只能接收 HumanName 参数
 let f =
     function
     | HumanName(n) -> printfn "%s" n
