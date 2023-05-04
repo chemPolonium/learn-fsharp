869e957605f3bc285f478cc9d46dcb3083c2d0b5
chemPolonium <chempolonium@outlook.com>
2023-05-04 17:39:46 +0800

    27 comment

--- lt27/Program.fs
+++ lt27/Program.fs
@@ -1,4 +1,8 @@
-﻿open Module1
+﻿// 27 使用模块可以让项目更有序
+
+// 使用 open 打开项目内的模块
+// 项目内 Module1 需要排在 Program 前面才能找到
+open Module1
 
 let a = [ 1..10 ]
 let b = [| 1..10 |]
@@ -6,4 +10,5 @@ let b = [| 1..10 |]
 for i in a do
     printfn "%d," i
 
+// printInt 来自 Module1
 a |> Seq.iter printInt
